6585b10963d64dfd232b688044ddfd38bfcb4394
malujanr-max <malujanr@unal.edu.co>
2025-11-04 18:04:09 -0500

    Add files via upload

---
+++ Ejercicio_4_418.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_4_418;
+
+/**
+ *
+ * @author Mateo
+ */
+public class Ejercicio_4_418 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal formulario = new Principal();
+        formulario.setVisible(true);
+    }
+    
+}
---
+++ Programador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio_4_418;
+
+
+public class Programador {
+   String nombre;
+    String apellidos;
+    Programador(String nombre, String apellidos){ 
+    this.nombre=nombre;
+    this.apellidos = apellidos; 
+}
+}
